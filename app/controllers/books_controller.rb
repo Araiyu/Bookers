95e2940e8520d4e6768b5fab7ea6a1ff71f76455
新井　雄介 <[ay088055@gmail.com]>
2019-06-10 19:52:59 +0900

    F-create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,23 +3,30 @@ class BooksController < ApplicationController
   end
 
   def index
+  	#記事の取得
+  	# @books = Book.all
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def new
-  		@book =Book.new
+  	#空のモデルをビューへ渡す
+  		@book = Book.new
   end
 
   def create
-  	book = Book.new(book_paramas)
+  	book = Book.new(book_params)
   	book.save
   	redirect_to books_path
   end
 
   def edit
   end
+
+
 
   	private
   	def book_params
