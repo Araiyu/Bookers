c34e40332bc1a96608bff4f43142e04b34cc3ffe
新井　雄介 <[ay088055@gmail.com]>
2019-06-11 19:32:56 +0900

    F-bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,8 +19,10 @@ class BooksController < ApplicationController
 
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	if book.save
+  		flash[:notice] = "successfully"
+  		redirect_to book_path(book.id)
+  	end
   end
 
   def edit
@@ -29,8 +31,10 @@ class BooksController < ApplicationController
 
   def update
   	  book = Book.find(params[:id])
-  	  book.update(book_params)
-  	  redirect_to book_path(book)
+  	  if book.update(book_params)
+  	  	flash[:notice] = "successfully"
+  	  	 redirect_to book_path(book)
+  	  	end
   end
 
  def destroy
